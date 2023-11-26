280e38fed4146bfa41052bdb6d3a1e5bf9982925
Laurene Zyskind <laurene.zyskind@gmail.com>
2023-11-26 16:27:07 +0100

    edit ShoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,27 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from "../datas/plantList";
 
-function ShoppingList(){
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{ plant }</li>
-            ))}
-        </ul>
-    )
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name}</li>
+				))}
+			</ul>
+		</div>
+	)
 }
+
 
 export default ShoppingList
